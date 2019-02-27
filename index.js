ad22a39d15ca5765eebd9629c0bd2bfe52ae7343
Amrith <siminskimark@gmail.com>
2019-02-26 20:54:26 -0500

    2/25/19
    ....

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 window.onscroll = function() {moveTitle()};
 
-
+var displayCheck = document.getElementById('divider').style.display
 var header = document.getElementById('titlebar');
 
 var sticky = header.offsetTop;
+var stickyMobile = document.getElementById("Nav").offsetTop;
 
 function moveTitle() {
-  if (window.pageYOffset > sticky){
-    header.classList.add("stickyH");
-    //window.alert("JS Functioning!");
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  //  if (window.pageYOffset > sticky){
+    //  document.getElementById('Nav').classList.add("stickyH");
+  //  }else {
+    //  document.getElementById('Nav').classList.remove("stickyH");
+    //}
+
   }else {
-    header.classList.remove("stickyH");
+    if (window.pageYOffset > sticky ){
+      header.classList.add("stickyH");
+      //window.alert("JS Functioning!");
+    }else {
+      header.classList.remove("stickyH");
+    }
   }
 }
